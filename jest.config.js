d9d88c74584e9f076b4e625fe5dd478beda9b0a5
Peter Patocka <patocka.peter@gmail.com>
2024-12-27 17:28:37 +0100

    Updates

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('jest').Config} */
-const config = {
-    modulePathIgnorePatterns: ['<rootDir>/build/'],
-    moduleFileExtensions: ['web.js', 'js', 'jsx', 'web.ts', 'ts', 'web.tsx', 'tsx', 'json', 'web.jsx', 'node'],
-};
-
-module.exports = config;
--- src/containers/App.test.js
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react';
-
-import App from './App';
-
-test('renders learn react link', () => {
-  const { getByTestId } = render(<App />);
-  const linkElement = getByTestId("header");
-  expect(linkElement).toBeInTheDocument();
-});
