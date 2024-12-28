4120c6858c8c2fddb25dbb9333fa8f857b4a3e8a
Peter Patocka <patocka.peter@gmail.com>
2024-12-28 11:09:01 +0100

    Cleanup unused files

--- src/js/images.js
+++ src/js/images.js
@@ -1,7 +1,6 @@
 import heroWallpaper from '../assets/images/welcome-wallpaper.jpg';
 import webDevWallpaper from '../assets/images/web-development-wallpaper.jpg';
 import generalProjectsWallpaper from '../assets/images/general-projects-wallpaper.jpg';
-import multimediaWallpaper from '../assets/images/multimedia-wallpaper.jpg';
 import artWalppaper from '../assets/images/art-wallpaper.jpg';
 import aboutWalppaper from '../assets/images/about-wallpaper.jpg';
 import profilePicture from '../assets/images/me.png';
@@ -10,7 +9,6 @@ export {
   heroWallpaper,
   webDevWallpaper,
   generalProjectsWallpaper,
-  multimediaWallpaper,
   artWalppaper,
   aboutWalppaper,
   profilePicture
